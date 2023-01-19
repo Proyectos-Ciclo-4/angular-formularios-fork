e8ec27aeb733a9a7f5695a17739b8e13f6859cfe
Julian Lasso <julian.lasso@sofka.com.co>
2023-01-19 11:29:00 -0500

    Formulario en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formularios';
+  frmFormulario: FormGroup;
+
+  constructor() {
+    this.frmFormulario = new FormGroup({
+      user: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.pattern('^((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[a-z])(?=.*[^\w\d\s])).{7,30}$')]),
+      confirmPassword: new FormControl(null, [Validators.required, Validators.pattern('^((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[a-z])(?=.*[^\w\d\s])).{7,30}$')]),
+    });
+  }
+
+  viewForm(): void {
+    console.log(this.frmFormulario);
+  }
+
+  send(): void {
+    console.log(this.frmFormulario.getRawValue());
+  }
+
 }
